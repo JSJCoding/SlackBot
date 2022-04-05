f059803a8ab93dcb15fb18c105967e3b4789f81e
JSJCoding <jonas.stryhn@gmail.com>
2022-04-05 16:28:40 +0200

    inital commit

---
+++ config.py
@@ -0,0 +1,22 @@
+import configparser
+
+CONFIG = None
+
+
+def read_config():
+    global CONFIG
+    if CONFIG is None:
+        CONFIG = configparser.ConfigParser()
+        CONFIG.read("config.ini")
+
+
+def slack_config():
+    read_config()
+    return {
+            "debug": CONFIG.get("slack", "debug", fallback=True),
+            "slack_token": CONFIG.get("slack", "token", fallback=""),
+            "channel_id": CONFIG.get("slack", "channel_id", fallback=""),
+            "channels": {
+                    "test": "xxxx"
+                }
+    }
---
+++ main.py
@@ -0,0 +1,36 @@
+# https://slack.dev/python-slack-sdk/web/index.html#messaging
+# https://medium.com/slack-developer-blog/bringing-your-bot-into-threaded-messages-cd272a42924f
+# https://api.slack.com/apps/A03914BTBUG/oauth?success=1
+
+import slack
+from datetime import date, timedelta
+
+
+def get_keycode(today):
+    new_code = old_code = None
+    with open("test.txt") as keyfile:
+        for key in keyfile:
+            date, recipient, sender, subject, code = key.split('|')
+            if date == str(today):
+                code = code.split(';')
+                new_code = ''.join(filter(str.isdigit, code[0]))
+                old_code = ''.join(filter(str.isdigit, code[1]))
+
+    return new_code, old_code
+
+
+next_post = today = date.today()
+if next_post != today:
+    slack.post_message("test", "There shall be no post today!")
+    exit()
+
+new_code, old_code = get_keycode(today)
+if new_code is None or old_code is None:
+    slack.post_message(
+            "test", "Did not find any keys for today, help me find them!")
+    exit()
+
+slack.post_message(
+        "test", f"Havne keys has been updated: \n {old_code} -> {new_code}")
+
+next_post += timedelta(days=7)
